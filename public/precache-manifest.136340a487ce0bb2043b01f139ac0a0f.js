06596fd48b9575c2a6bfec57ab3ae7d2db467229
Jennifer Pazos <jmp521@gmail.com>
2021-11-23 21:15:11 -0600

    added build files to public folder

---
+++ public/precache-manifest.136340a487ce0bb2043b01f139ac0a0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e153453b85514c4fe2ab2e36fa943dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5269222a3220e58a2e",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "528da4066277f2835eba",
+    "url": "/static/js/2.3dc10976.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/static/js/2.3dc10976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c1f5d7d2121adea7b0",
+    "url": "/static/js/3.06851143.chunk.js"
+  },
+  {
+    "revision": "fe5269222a3220e58a2e",
+    "url": "/static/js/main.f99ed6df.chunk.js"
+  },
+  {
+    "revision": "0ea52b760fa025861cb5",
+    "url": "/static/js/runtime-main.b03bedd2.js"
+  }
+]);
